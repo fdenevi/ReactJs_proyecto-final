5537e3a61274f82f3cd5c78f54b6fd1b7ca36f2d
Florencia Denevi <fdenevi@hotmail.com>
2022-12-04 22:37:54 -0300

    botones en productos

--- src/components/Productos/Productos.jsx
+++ src/components/Productos/Productos.jsx
@@ -1,9 +1,22 @@
 import { useEffect, useState } from "react"
+import { Spinner } from "react-bootstrap"
 import { gFetch } from "../../helpers/gfetch"
 import './Productos.css'
 
 const Productos = () => {
 
+  const iva = 1.21
+  
+  // CONSTANTE BOTONES
+    const [contador, setearAgregar] = useState(0)
+    const agregarAcarrito = () => {
+        setearAgregar (contador + 1)
+    }
+    const quitarDelCarrito = () => {
+      setearAgregar (contador - 1)
+    }
+
+  // PRODUCTOS
     const [products, setProducts] = useState ([])  
     const [loading, setLoading] = useState (true)
   
@@ -17,16 +30,22 @@ const Productos = () => {
   return (
     <div className='container' id= "cardProds">
 
-        {loading ? <p>Cargando productos...</p> 
-        : products.map ( prods => 
+        {loading ?  <div>
+                      <p>Cargando productos</p>
+                      <Spinner animation="grow" />
+                    </div> 
+        : products.map ( prods =>
                 <div key = {prods.id} className="cards">
                     <img src={prods.foto} alt="img producto" />
                     <p className="titleProds">{prods.name}</p>
-                    <button className="btnCardWidget">Agregar al carrito</button>
-                </div> 
+                    <p>{prods.price * iva}</p>
+                    <button className="btnCardWidget">Mas detalles</button>
+                    <button onClick={agregarAcarrito}>Añadir al carrito</button>
+                    <button onClick={quitarDelCarrito}>Quitar del carrito</button>
+                    <p>{contador}</p>
+                </div>
             )
-        }
-        
+        }       
     </div>
   )
 }
