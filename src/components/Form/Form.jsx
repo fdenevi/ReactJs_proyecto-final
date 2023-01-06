76510cd964e9f83e7c540446932bceeae55c1daf
Florencia Denevi <fdenevi@hotmail.com>
2023-01-06 10:45:41 -0300

    validacion de email

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -6,63 +6,63 @@ const Form = ({ endBuy, handleOnChange, dataForm }) => {
 
   return (
     <form onSubmit={endBuy} id="form">
-    <div className="inputForm">
-      <div>
-        <label htmlFor="name">Nombre y Apellido:</label>
-        <input
-          type="text"
-          className="formControl"
-          name="name"
-          value={dataForm.name}
-          placeholder="nombre y apellido"
-          onChange={handleOnChange}
-          required
-        />
+      <div className="inputForm">
+        <div>
+          <label htmlFor="name">Nombre y Apellido:</label>
+          <input
+            type="text"
+            className="formControl"
+            name="name"
+            value={dataForm.name}
+            placeholder="nombre y apellido"
+            onChange={handleOnChange}
+            required
+          />
+        </div>
+
+        <div>
+          <label htmlFor="email">Email:</label>
+          <input 
+            type="text" 
+            className="formControl"
+            name="email"
+            value={dataForm.email}
+            placeholder="email"
+            onChange={handleOnChange}
+            required
+          />
+        </div>
+
+        <div>
+          <label htmlFor="email2">Confirmar email:</label>
+          <input 
+            type="text" 
+            className="formControl"
+            name="email2"
+            value={dataForm.email2}
+            placeholder="confirmar email"
+            onChange={handleOnChange}
+            required
+          />
+        </div>
+
+        <div>
+          <label htmlFor="phone">Celular:</label>
+          <input 
+            type="number" 
+            className="formControl"
+            name="phone"
+            value={dataForm.phone}
+            placeholder="celular"
+            onChange={handleOnChange}
+            required
+          />
+        </div>
       </div>
 
-      <div>
-        <label htmlFor="email">Email:</label>
-        <input 
-          type="text" 
-          className="formControl"
-          name="email"
-          value={dataForm.email}
-          placeholder="email"
-          onChange={handleOnChange}
-          required
-        />
-      </div>
+      <FormButton />
 
-      <div>
-        <label htmlFor="email">Confirmar email:</label>
-        <input 
-          type="text" 
-          className="formControl"
-          name="email"
-          value={dataForm.email}
-          placeholder="email"
-          onChange={handleOnChange}
-          required
-        />
-      </div>
-
-      <div>
-        <label htmlFor="phone">Celular:</label>
-        <input 
-          type="number" 
-          className="formControl"
-          name="phone"
-          value={dataForm.phone}
-          placeholder="celular"
-          onChange={handleOnChange}
-          required
-        />
-      </div>
-    </div>
-
-    <FormButton />
-
-  </form>
+    </form>
   )
 }
 
--- src/components/FormList/FormList.jsx
+++ src/components/FormList/FormList.jsx
@@ -10,6 +10,7 @@ const FormList = () => {
     const [dataForm, setDataForm] = useState({
         name: "",
         email: "",
+        email2: "",
         phone: "",
       });
 
@@ -23,28 +24,37 @@ const FormList = () => {
         order.item = cartList.map( ({name, id, price}) => ({name, id, price}) )
         order.total = totalPrice()
 
+        
         const db = getFirestore()
-
         const queryOrder = collection(db, 'orders')
-        addDoc (queryOrder, order)
-        .then (resp => 
+        if(dataForm.email2 === dataForm.email){
+          addDoc (queryOrder, order)
+          .then (resp =>
+            Swal.fire({
+              icon: 'success',
+              title: 'COMPRA REALIZADA CON ÉXITO!',
+              text: 'Su número de orden es:',
+              footer: resp.id,
+              showConfirmButton: false,
+            }) 
+          )
+          .catch(err => console.log(err))
+          .finally(() => {
+            cleanCart()
+            setDataForm({
+              name: "",
+              email: "",
+              email2: "",
+              phone: "",
+            })
+          })       
+        } else {
           Swal.fire({
-            icon: 'success',
-            title: 'COMPRA REALIZADA CON ÉXITO!',
-            text: 'Su número de orden es:',
-            footer: resp.id,
-            showConfirmButton: false,
-          })
-        )
-        .catch(err => console.log(err))
-        .finally(() => {
-          cleanCart()
-          setDataForm({
-            name: "",
-            email: "",
-            phone: "",
-          })          
-        })
+            icon: 'error',
+            title: 'Oops...',
+            text: 'El mail debe coincidir',
+          }) 
+        }
     }
 
     const handleOnChange = (e) => {
